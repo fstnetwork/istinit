6b86ded5966f09e78813107cf122a69b658e3298
xrelkd <wei@fstk.io>
2020-11-19 18:20:01 +0800

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,30 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  inherit (pkgs) stdenv;
+in pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+
+    git
+    drone-cli
+
+    rustup
+    cargo-make
+
+    clang
+    llvmPackages.libclang
+    pkgconfig
+
+    openssl
+
+    kubectl
+    kind
+    istio
+  ];
+
+  RUST_BACKTRACE = 1;
+
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+
+  KUBECONFIG = "./target/kubeconfig";
+}
